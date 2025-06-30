ebc938011f20240875e1b746a36696e02915d928
student-crative <aryasutariya3@gmail.com>
2025-06-30 18:36:28 +0530

    Student Result In React

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import StudentResult from './StudentResult';
+import Homepage from './Homepage';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <StudentResult /> */}
+      <Homepage />
+    </div>
+  );
+}
+
+export default App;
